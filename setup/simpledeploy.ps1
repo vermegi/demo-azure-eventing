9c1c87126cfba5da5f2d6331ef5c90f4a2e8ac16
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2017-02-02 15:22:18 +0100

    branch to param

--- setup/simpledeploy.ps1
+++ setup/simpledeploy.ps1
@@ -1,7 +1,10 @@
-$ResourceGroupLocation = 'westeurope'
-$ResourceGroupName = 'azure-eventing-demo'
-$TemplateFile = 'azuredeploy.json'
-$TemplateParametersFile = 'azuredeploy.parameters.json'
+Param(
+    [string] $ResourceGroupLocation = 'westeurope',
+    [string] $ResourceGroupName = 'azure-eventing-demo',
+    [string] $TemplateFile = 'azuredeploy.json',
+    [string] $TemplateParametersFile = 'azuredeploy.parameters.json',
+    [string] $Branch = 'master'
+)
 
 # Create or update the resource group using the specified template file and template parameters file
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Force
@@ -12,5 +15,6 @@ New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName
                                     -ResourceGroupName $ResourceGroupName `
                                     -TemplateFile $TemplateFile `
                                     -TemplateParameterFile $TemplateParametersFile `
+                                    -Branch $Branch
                                     -Force -Verbose `
                                     -Mode Incremental 
